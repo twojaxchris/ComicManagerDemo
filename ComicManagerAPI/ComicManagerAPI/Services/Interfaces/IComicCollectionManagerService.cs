593febd6e96c118e0678dce1400ce6e730c490e8
Chris Smith <ctlsmithis@gmail.com>
2017-10-23 20:50:09 -0400

    Added interface for collection manager service

---
+++ ComicManagerAPI/ComicManagerAPI/Services/Interfaces/IComicCollectionManagerService.cs
@@ -0,0 +1,16 @@
+﻿using ComicManagerAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicManagerAPI.Services.Interfaces
+{
+    interface IComicCollectionManagerService
+    {
+        List<Comic> AddComicToCollection(Comic comicToAdd);
+        List<Comic> RemoveComicFromCollection(Comic comicToRemove);
+        List<Comic> UpdateComicInCollection(Comic comicToRemove);
+        List<Comic> SearchComicCollection(string name, int issue_number);
+    }
+}
