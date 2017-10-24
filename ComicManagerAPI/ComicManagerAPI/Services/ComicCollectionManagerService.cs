171ee1fcbbc531ced4534c61f4289f748122dcc1
Chris Smith <ctlsmithis@gmail.com>
2017-10-23 20:50:34 -0400

    Initial add for collection manager service implementation

---
+++ ComicManagerAPI/ComicManagerAPI/Services/ComicCollectionManagerService.cs
@@ -0,0 +1,32 @@
+﻿using ComicManagerAPI.Models;
+using ComicManagerAPI.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicManagerAPI.Services
+{
+    public class ComicCollectionManagerService : IComicCollectionManagerService
+    {
+        public List<Comic> AddComicToCollection(Comic comicToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comic> RemoveComicFromCollection(Comic comicToRemove)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comic> UpdateComicInCollection(Comic comicToRemove)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comic> SearchComicCollection(string name, int issue_number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
