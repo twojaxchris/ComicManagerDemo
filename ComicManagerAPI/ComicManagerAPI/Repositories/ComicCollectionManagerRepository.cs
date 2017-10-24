dddca98260e58886e880dccbad19c581544de4af
Chris Smith <ctlsmithis@gmail.com>
2017-10-23 22:56:51 -0400

    Put in some test repo stuff for a second

--- ComicManagerAPI/ComicManagerAPI/Repositories/ComicCollectionManagerRepository.cs
+++ ComicManagerAPI/ComicManagerAPI/Repositories/ComicCollectionManagerRepository.cs
@@ -9,21 +9,63 @@ namespace ComicManagerAPI.Repositories
 {
     public class ComicCollectionManagerRepository : IComicCollectionManagerRepository
     {
+        private List<Comic> blah;
+
+        public ComicCollectionManagerRepository()
+        {
+            blah = new List<Comic>();
+
+            blah.Add(new Comic
+            {
+                title = "Test Comic 1",
+                url = "www.google.com",
+                coverDate = "10/23/2017",
+                imageURL = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                issueNumber = "1"
+            });
+            blah.Add(new Comic
+            {
+                title = "Test Comic 2",
+                url = "www.google.com",
+                coverDate = "10/23/2017",
+                imageURL = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                issueNumber = "2"
+            });
+            blah.Add(new Comic
+            {
+                title = "Test Comic 3",
+                url = "www.google.com",
+                coverDate = "10/23/2017",
+                imageURL = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+                issueNumber = "3"
+            });
+
+        }
+
         public List<Comic> AddComicToCollection(Comic comicToAdd, string userName)
         {
-            throw new NotImplementedException();
+            blah.Add(comicToAdd);
+            return blah;
         }
         public List<Comic> RemoveComicFromCollection(Comic comicToRemove, string userName)
         {
-            throw new NotImplementedException();
+            blah.Remove(comicToRemove);
+            return blah;
         }
-        public List<Comic> UpdateComicInCollection(Comic comicToRemove, string userName)
+        public List<Comic> UpdateComicInCollection(Comic originalComic, Comic newComic, string userName)
         {
-            throw new NotImplementedException();
+            Comic resultComic = blah.Find(x => x == originalComic);
+            resultComic.title = newComic.title;
+            resultComic.issueNumber = newComic.issueNumber;
+            resultComic.imageURL = newComic.imageURL;
+            resultComic.url = newComic.url;
+            resultComic.coverDate = newComic.coverDate;
+            return blah;
         }
         public List<Comic> SearchComicCollection(string name, int issue_number, string userName)
         {
-            throw new NotImplementedException();
+            List<Comic> blahFound = blah.Where(x => x.title == name && x.issueNumber == issue_number.ToString()).ToList();
+            return blahFound;
         }
     }
 }
--- ComicManagerAPI/ComicManagerAPI/Repositories/Interfaces/IComicCollectionManagerRepository.cs
+++ ComicManagerAPI/ComicManagerAPI/Repositories/Interfaces/IComicCollectionManagerRepository.cs
@@ -10,7 +10,7 @@ namespace ComicManagerAPI.Repositories.Interfaces
     {
         List<Comic> AddComicToCollection(Comic comicToAdd, string userName);
         List<Comic> RemoveComicFromCollection(Comic comicToRemove, string userName);
-        List<Comic> UpdateComicInCollection(Comic comicToRemove, string userName);
+        List<Comic> UpdateComicInCollection(Comic originalComic, Comic newComic, string userName);
         List<Comic> SearchComicCollection(string name, int issue_number, string userName);
     }
 }
